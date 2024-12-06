5ff32fd525227f444687f5237b2f876eed884de9
selgas <s.elgas@techsmith.com>
2024-12-06 11:39:39 -0500

    Display tile images in a listview

--- TileMapEditor/TileMapEditor/ViewModels/MainWindowViewModel.cs
+++ TileMapEditor/TileMapEditor/ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,39 @@
 ﻿using DeviceFrameEditor.ViewModels;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows.Media.Imaging;
 using TileMapEditor.Tiles;
 
 namespace TileMapEditor.ViewModels
 {
    internal class MainWindowViewModel : ViewModelBase
    {
-      private TileSet _tileSet;
+      private readonly TileSet _tileSet;
+
+      public ObservableCollection<TileViewData> TileViewDatas { get; } = [];
 
       public MainWindowViewModel()
       {
          _tileSet = new TileSet( "..\\..\\..\\..\\..\\Assets\\tile_textures.png" );
+
+         for ( int i = 0; i < Constants.TileCount; i++ )
+         {
+            var image = new BitmapImage();
+
+            using var stream = new MemoryStream();
+            {
+               var encoder = new PngBitmapEncoder();
+               encoder.Frames.Add( BitmapFrame.Create( _tileSet.TileBitmaps[i] ) );
+               encoder.Save( stream );
+               image.BeginInit();
+               image.CacheOption = BitmapCacheOption.OnLoad;
+               image.StreamSource = stream;
+               image.EndInit();
+               image.Freeze();
+            }
+
+            TileViewDatas.Add( new( i, image ) );
+         }
       }
    }
 }
