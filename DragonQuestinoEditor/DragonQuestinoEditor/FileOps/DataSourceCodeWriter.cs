8fdbf43091b2df551b5c0d4c913792d4469d6e76
selgas <s.elgas@techsmith.com>
2024-12-20 16:53:40 -0500

    Use file stream instead of string building
    WOW, this is an EXTREME performance improvement.

--- DragonQuestinoEditor/DragonQuestinoEditor/FileOps/DataSourceCodeWriter.cs
+++ DragonQuestinoEditor/DragonQuestinoEditor/FileOps/DataSourceCodeWriter.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.IO;
+using System.Text;
 using DragonQuestinoEditor.Graphics;
 using DragonQuestinoEditor.ViewModels;
 
@@ -14,52 +15,50 @@ namespace DragonQuestinoEditor.FileOps
       private readonly TileSet _tileSet = tileSet;
       private readonly ObservableCollection<TileViewModel> _tiles = mapTiles;
       private readonly SpriteSheet _spriteSheet = spriteSheet;
-      private string _fileContents = string.Empty;
 
       public void WriteFile( string filePath )
       {
-         BuildHeaderSection();
-         BuildPaletteFunction();
-         BuildTileTexturesFunction();
-         BuildTileMapFunction();
-         BuildSpriteFunctions();
-
-         File.WriteAllText( filePath, _fileContents );
+         using FileStream fs = File.Create( filePath );
+         WriteHeaderSection( fs );
+         WritePaletteFunction( fs );
+         WriteTileTexturesFunction( fs );
+         WriteTileMapFunction( fs );
+         WriteSpriteFunctions( fs );
       }
 
-      private void BuildHeaderSection()
+      private void WriteHeaderSection( FileStream fs )
       {
-         _fileContents = "// THIS FILE IS AUTO-GENERATED, PLEASE DO NOT MODIFY!\n\n";
-         _fileContents += "#include \"screen.h\"\n";
-         _fileContents += "#include \"tile_map.h\"\n";
-         _fileContents += "#include \"sprite.h\"\n";
+         WriteText( fs, "// THIS FILE IS AUTO-GENERATED, PLEASE DO NOT MODIFY!\n\n" );
+         WriteText( fs, "#include \"screen.h\"\n" );
+         WriteText( fs, "#include \"tile_map.h\"\n" );
+         WriteText( fs, "#include \"sprite.h\"\n" );
       }
 
-      private void BuildPaletteFunction()
+      private void WritePaletteFunction( FileStream fs )
       {
-         _fileContents += "\nvoid Screen_LoadPalette( Screen_t* screen )\n";
-         _fileContents += "{\n";
-         _fileContents += "   uint16_t i;\n\n";
-         _fileContents += string.Format( "   for ( i = 0; i < {0}; i++ ) {{ screen->palette[i] = 0; }}\n\n", Constants.PaletteSize );
+         WriteText( fs, "\nvoid Screen_LoadPalette( Screen_t* screen )\n" );
+         WriteText( fs, "{\n" );
+         WriteText( fs, "   uint16_t i;\n\n" );
+         WriteText( fs, string.Format( "   for ( i = 0; i < {0}; i++ ) {{ screen->palette[i] = 0; }}\n\n", Constants.PaletteSize ) );
 
          for ( int i = 0; i < _palette.ColorCount; i++ )
          {
-            _fileContents += string.Format( "   screen->palette[{0}] = 0x{1};\n", i, _palette.Colors[i].ToString( "X4" ) );
+            WriteText( fs, string.Format( "   screen->palette[{0}] = 0x{1};\n", i, _palette.Colors[i].ToString( "X4" ) ) );
          }
 
-         _fileContents += "}\n";
+         WriteText( fs, "}\n" );
       }
 
-      private void BuildTileTexturesFunction()
+      private void WriteTileTexturesFunction( FileStream fs )
       {
-         _fileContents += "\nvoid TileMap_LoadTextures( TileMap_t* tileMap )\n";
-         _fileContents += "{\n";
-         _fileContents += "   uint32_t* mem32;\n\n";
+         WriteText( fs, "\nvoid TileMap_LoadTextures( TileMap_t* tileMap )\n" );
+         WriteText( fs, "{\n" );
+         WriteText( fs, "   uint32_t* mem32;\n\n" );
 
          // TODO: try compressing this, it only ever gets called once
          for ( int i = 0; i < Constants.TileTextureCount; i++ )
          {
-            _fileContents += string.Format( "   mem32 = (uint32_t*)( tileMap->textures[{0}].memory );\n", i );
+            WriteText( fs, string.Format( "   mem32 = (uint32_t*)( tileMap->textures[{0}].memory );\n", i ) );
 
             var pixelIndexes = _tileSet.TilePaletteIndexes[i];
 
@@ -72,26 +71,26 @@ namespace DragonQuestinoEditor.FileOps
 
                var packed = ( index3 << 24 ) | ( index2 << 16  ) | ( index1 << 8  ) | ( index0 << 0  );
 
-               _fileContents += string.Format( "   mem32[{0}] = 0x{1};\n", memoryIndex, packed.ToString( "X8" ) );
+               WriteText( fs, string.Format( "   mem32[{0}] = 0x{1};\n", memoryIndex, packed.ToString( "X8" ) ) );
             }
          }
 
-         _fileContents += "}\n";
+         WriteText( fs, "}\n" );
       }
 
-      private void BuildTileMapFunction()
+      private void WriteTileMapFunction( FileStream fs )
       {
-         _fileContents += "\nvoid TileMap_Load( TileMap_t* tileMap, uint32_t index )\n";
-         _fileContents += "{\n";
-         _fileContents += "   int32_t i;\n";
-         _fileContents += "   uint32_t* tiles32 = (uint32_t*)( tileMap->tiles );\n\n";
-         _fileContents += "   switch( index )\n";
-         _fileContents += "   {\n";
-         _fileContents += "      case 0:\n";
-         _fileContents += string.Format( "         tileMap->tilesX = {0};\n", Constants.TileMapTileCountX );
-         _fileContents += string.Format( "         tileMap->tilesY = {0};\n", Constants.TileMapTileCountY );
-         _fileContents += "         for ( i = 0; i < TILEMAP_MAX_PORTALS; i++ ) {{ tileMap->portals[i].sourceTileIndex = -1; }}\n";
-         _fileContents += string.Format( "         tileMap->spriteCount = 0;\n" );
+         WriteText( fs, "\nvoid TileMap_Load( TileMap_t* tileMap, uint32_t index )\n" );
+         WriteText( fs, "{\n" );
+         WriteText( fs, "   int32_t i;\n" );
+         WriteText( fs, "   uint32_t* tiles32 = (uint32_t*)( tileMap->tiles );\n\n" );
+         WriteText( fs, "   switch( index )\n" );
+         WriteText( fs, "   {\n" );
+         WriteText( fs, "      case 0:\n" );
+         WriteText( fs, string.Format( "         tileMap->tilesX = {0};\n", Constants.TileMapTileCountX ) );
+         WriteText( fs, string.Format( "         tileMap->tilesY = {0};\n", Constants.TileMapTileCountY ) );
+         WriteText( fs, "         for ( i = 0; i < TILEMAP_MAX_PORTALS; i++ ) {{ tileMap->portals[i].sourceTileIndex = -1; }}\n" );
+         WriteText( fs, string.Format( "         tileMap->spriteCount = 0;\n" ) );
 
          var packedTiles = new List<UInt32>( _tiles.Count / 2 );
          var indexCounts = new Dictionary<UInt32, int>();
@@ -136,27 +135,27 @@ namespace DragonQuestinoEditor.FileOps
             }
          }
 
-         _fileContents += string.Format( "         for ( i = 0; i < ( TILE_COUNT / 2 ); i++ ) {{ tiles32[i] = 0x{0}; }}\n", mostCommonValue.ToString( "X8" ) );
+         WriteText( fs, string.Format( "         for ( i = 0; i < ( TILE_COUNT / 2 ); i++ ) {{ tiles32[i] = 0x{0}; }}\n", mostCommonValue.ToString( "X8" ) ) );
 
          for ( int i = 0, packedTileIndex = 0; i < _tiles.Count; i += 2, packedTileIndex++ )
          {
             if ( packedTiles[packedTileIndex] != mostCommonValue )
             {
-               _fileContents += string.Format( "         tiles32[{0}] = 0x{1};\n", packedTileIndex, packedTiles[packedTileIndex].ToString( "X8" ) );
+               WriteText( fs, string.Format( "         tiles32[{0}] = 0x{1};\n", packedTileIndex, packedTiles[packedTileIndex].ToString( "X8" ) ) );
             }
          }
 
-         _fileContents += "         break;\n";
-         _fileContents += "   }\n";
-         _fileContents += "}\n";
+         WriteText( fs, "         break;\n" );
+         WriteText( fs, "   }\n" );
+         WriteText( fs, "}\n" );
       }
 
-      private void BuildSpriteFunctions()
+      private void WriteSpriteFunctions( FileStream fs )
       {
-         _fileContents += "\nvoid Sprite_LoadPlayer( Sprite_t* sprite )\n";
-         _fileContents += "{\n";
-         _fileContents += "   int32_t i;\n";
-         _fileContents += "   uint32_t* mem32 = (uint32_t*)( sprite->textures[0].memory );\n\n";
+         WriteText( fs, "\nvoid Sprite_LoadPlayer( Sprite_t* sprite )\n" );
+         WriteText( fs, "{\n" );
+         WriteText( fs, "   int32_t i;\n" );
+         WriteText( fs, "   uint32_t* mem32 = (uint32_t*)( sprite->textures[0].memory );\n\n" );
 
          var indexCounts = new Dictionary<UInt32, int>();
          var packedIndexes = new List<UInt32>();
@@ -201,13 +200,13 @@ namespace DragonQuestinoEditor.FileOps
             }
          }
 
-         _fileContents += string.Format( "   for ( i = 0; i < ( SPRITE_TEXTURE_BYTES / 4 ) * SPRITE_TEXTURES; i++ ) {{ mem32[i] = 0x{0}; }}\n", mostCommonValue.ToString( "X8" ) );
+         WriteText( fs, string.Format( "   for ( i = 0; i < ( SPRITE_TEXTURE_BYTES / 4 ) * SPRITE_TEXTURES; i++ ) {{ mem32[i] = 0x{0}; }}\n", mostCommonValue.ToString( "X8" ) ) );
 
          for ( int i = 0, packedIndex = 0; i < Constants.SpritePositionCount; i++ )
          {
             for ( int j = 0; j < Constants.SpriteFrameCount; j++ )
             {
-               _fileContents += string.Format( "   mem32 = (uint32_t*)( sprite->textures[{0}].memory );\n", ( i * Constants.SpriteFrameCount ) + j );
+               WriteText( fs, string.Format( "   mem32 = (uint32_t*)( sprite->textures[{0}].memory );\n", ( i * Constants.SpriteFrameCount ) + j ) );
 
                var pixelIndexes = _spriteSheet.FramePaletteIndexes[i][j];
 
@@ -215,20 +214,26 @@ namespace DragonQuestinoEditor.FileOps
                {
                   if ( packedIndexes[packedIndex] != mostCommonValue )
                   {
-                     _fileContents += string.Format( "   mem32[{0}] = 0x{1};\n", memoryIndex, packedIndexes[packedIndex].ToString( "X8" ) );
+                     WriteText( fs, string.Format( "   mem32[{0}] = 0x{1};\n", memoryIndex, packedIndexes[packedIndex].ToString( "X8" ) ) );
                   }
                }
             }
          }
 
-         _fileContents += "}\n\n";
+         WriteText( fs, "}\n\n" );
 
          // TODO
-         _fileContents += "\nvoid Sprite_LoadGeneric( Sprite_t* sprite, uint32_t index )\n";
-         _fileContents += "{\n";
-         _fileContents += "   UNUSED_PARAM( sprite );\n";
-         _fileContents += "   UNUSED_PARAM( index );\n";
-         _fileContents += "}\n";
+         WriteText( fs, "\nvoid Sprite_LoadGeneric( Sprite_t* sprite, uint32_t index )\n" );
+         WriteText( fs, "{\n" );
+         WriteText( fs, "   UNUSED_PARAM( sprite );\n" );
+         WriteText( fs, "   UNUSED_PARAM( index );\n" );
+         WriteText( fs, "}\n" );
+      }
+
+      private static void WriteText( FileStream fs, string value )
+      {
+         byte[] info = new UTF8Encoding( true ).GetBytes( value );
+         fs.Write( info, 0, info.Length );
       }
    }
 }
