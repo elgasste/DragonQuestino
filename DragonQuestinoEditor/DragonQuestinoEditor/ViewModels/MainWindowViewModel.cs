46ce22d4ab34423f33c2303e3c89cdc886a5091c
selgas <s.elgas@techsmith.com>
2025-01-09 15:00:04 -0500

    Fixed a bug with portal tile map size

--- DragonQuestinoEditor/DragonQuestinoEditor/ViewModels/MainWindowViewModel.cs
+++ DragonQuestinoEditor/DragonQuestinoEditor/ViewModels/MainWindowViewModel.cs
@@ -39,6 +39,8 @@ namespace DragonQuestinoEditor.ViewModels
 
                OnPropertyChanged( nameof( TileMapTextureListViewWidth ) );
                OnPropertyChanged( nameof( TileMapTextureListViewHeight ) );
+               OnPropertyChanged( nameof( TileMapPortalListViewWidth ) );
+               OnPropertyChanged( nameof( TileMapPortalListViewHeight ) );
             }
          }
       }
