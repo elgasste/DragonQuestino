5b6e29289ca9aed7155d78fc6d094413d584687d
Alex Novak <alexwnovak@gmail.com>
2025-01-11 14:21:28 -0500

    +Sprite class
    This abstraction is designed to hold image information in a format
    useful for WPF. This class can load PNGs from disk and convert their
    indexed palette color information to 32-bit colors, which is much easier
    to work with for UIs.
    
    This presents a GetPixel/SetPixel API with Color instances, making it
    really easy to work with, exempting the caller from reasoning through
    buffer offsets.
    
    As proof that it can read files, I added a SaveAsPng method to show that
    ingested paths can be exported as 32-bit PNGs.

---
+++ DragonQuestinoEditor/DragonQuestinoEditor/Graphics/Sprite.cs
@@ -0,0 +1,93 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace DragonQuestinoEditor.Graphics
+{
+   internal class Sprite
+   {
+      private readonly Color[,] _pixels;
+
+      public int Width { get; }
+      public int Height { get; }
+
+      public Sprite( int width, int height )
+      {
+         Width = width;
+         Height = height;
+
+         _pixels = new Color[width, height];
+      }
+
+      public static Sprite LoadFromFile( string path )
+      {
+         using var fileStream = new FileStream( path, FileMode.Open, FileAccess.Read );
+         var decoder = new PngBitmapDecoder( fileStream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default );
+         var bitmapFrame = decoder.Frames[0];
+
+         int stride = bitmapFrame.PixelWidth * bitmapFrame.Format.BitsPerPixel / 8;
+
+         var data = new byte[stride * bitmapFrame.PixelHeight];
+         bitmapFrame.CopyPixels( data, stride, 0 );
+
+         var sprite = new Sprite( bitmapFrame.PixelWidth, bitmapFrame.PixelHeight );
+         int sourceOffset = 0;
+
+         for (int y = 0; y < bitmapFrame.PixelHeight; y++)
+         {
+            for (int x = 0; x < bitmapFrame.PixelWidth; x++)
+            {
+               int pixelIndex = data[sourceOffset++];
+               var color = bitmapFrame.Palette.Colors[pixelIndex];
+               sprite.SetPixel( x, y, color );
+            }
+         }
+
+         return sprite;
+      }
+
+      public void SetPixel( int x, int y, Color color )
+      {
+         _pixels[x, y] = color;
+      }
+
+      public Color GetPixel( int x, int y )
+      {
+         return _pixels[x, y];
+      }
+
+      private byte[] ConvertToLinearBuffer()
+      {
+         var linearPixelBuffer = new byte[_pixels.Length * 4];
+         int offset = 0;
+
+         for (int y = 0; y < Height; y++)
+         {
+            for (int x = 0; x < Width; x++)
+            {
+               linearPixelBuffer[offset++] = _pixels[x, y].B;
+               linearPixelBuffer[offset++] = _pixels[x, y].G;
+               linearPixelBuffer[offset++] = _pixels[x, y].R;
+               linearPixelBuffer[offset++] = _pixels[x, y].A;
+            }
+         }
+
+         return linearPixelBuffer;
+      }
+
+      public void SaveAsPng( string path )
+      {
+         var linearPixelBuffer = ConvertToLinearBuffer();
+
+         var writableBitmap = new WriteableBitmap( Width, Height, 96, 96, PixelFormats.Bgra32, null );
+         writableBitmap.WritePixels( new Int32Rect( 0, 0, Width, Height ), linearPixelBuffer, Width * 4, 0 );
+
+         using var fileStream = new FileStream( path, FileMode.Create, FileAccess.Write );
+
+         var encoder = new PngBitmapEncoder();
+         encoder.Frames.Add( BitmapFrame.Create( writableBitmap ) );
+         encoder.Save( fileStream );
+      }
+   }
+}
