e9dab7d3170f6a9e7253cc8cf07522dfc63754f6
Alex Novak <alexwnovak@gmail.com>
2025-01-10 19:36:20 -0500

    +Custom panel that renders the images outside of the layout system
    This is a quick and dirty swing at a different approach. Laying out
    19000 tiles in the WPF layout system is real bad, and uses way more than
    we need. These are ultimately just images in a grid, so we can use that
    specificity to reduce the complexity a ton.
    
    This panel binds a collection of tile view models and iterates them and
    renders their images. This makes some assumptions, such as columns and
    rows, and ideally wouldn't know what a view model was. It also can't do
    scrolling or user interaction. But it's hella fast.

---
+++ DragonQuestinoEditor/DragonQuestinoEditor/MapPanel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Media;
+using DragonQuestinoEditor.ViewModels;
+
+namespace DragonQuestinoEditor
+{
+   internal class MapPanel : FrameworkElement
+   {
+      public static readonly DependencyProperty TileMapProperty = DependencyProperty.Register(
+         nameof( TileMap ),
+         typeof( Collection<TileViewModel> ),
+         typeof( MapPanel ),
+         new FrameworkPropertyMetadata( null, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsArrange ) );
+
+      public Collection<TileViewModel> TileMap
+      {
+         get => (Collection<TileViewModel>)GetValue( TileMapProperty );
+         set => SetValue( TileMapProperty, value );
+      }
+
+      protected override void OnRender( DrawingContext dc )
+      {
+         if (TileMap is null || TileMap.Count == 0)
+         {
+            return;
+         }
+
+         dc.DrawRectangle( Brushes.DarkSlateGray, null, new Rect( 0, 0, ActualWidth, ActualHeight ) );
+
+         for (int i = 0; i < TileMap.Count; i++)
+         {
+            var tile = TileMap[i];
+
+            int width = (int)tile.Image.Width;
+            int height = (int)tile.Image.Height;
+
+            int x = (i % 140) * width;
+            int y = (i / 140) * height;
+
+            dc.DrawImage( tile.Image, new Rect( x, y, width, height ) );
+         }
+      }
+   }
+}
