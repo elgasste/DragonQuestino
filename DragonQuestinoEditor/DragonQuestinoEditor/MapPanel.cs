800a649bf355581834f07f731933f554c7b07923
Alex Novak <alexwnovak@gmail.com>
2025-01-10 20:11:45 -0500

    Left button dragging now pans the image
    This is SUPER slow. If I limit the render to only paint 10 tiles, then
    it's super fast. Rendering 19000 tiles again is the bottleneck

--- DragonQuestinoEditor/DragonQuestinoEditor/MapPanel.cs
+++ DragonQuestinoEditor/DragonQuestinoEditor/MapPanel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
 using DragonQuestinoEditor.ViewModels;
 
@@ -7,6 +8,21 @@ namespace DragonQuestinoEditor
 {
    internal class MapPanel : FrameworkElement
    {
+      private bool _isLeftButtonDown;
+      private Point _dragAnchorPoint;
+
+      private static readonly DependencyProperty OffsetProperty = DependencyProperty.Register(
+         nameof( Offset ),
+         typeof( Vector ),
+         typeof( MapPanel ),
+         new FrameworkPropertyMetadata( new Vector( 0, 0 ), FrameworkPropertyMetadataOptions.AffectsRender ) );
+
+      public Vector Offset
+      {
+         get => (Vector)GetValue( OffsetProperty );
+         set => SetValue( OffsetProperty, value );
+      }
+
       public static readonly DependencyProperty TileMapProperty = DependencyProperty.Register(
          nameof( TileMap ),
          typeof( Collection<TileViewModel> ),
@@ -17,6 +33,30 @@ namespace DragonQuestinoEditor
       {
          get => (Collection<TileViewModel>)GetValue( TileMapProperty );
          set => SetValue( TileMapProperty, value );
+      }
+
+      protected override void OnMouseLeftButtonDown( MouseButtonEventArgs e )
+      {
+         base.OnMouseLeftButtonDown( e );
+
+         _isLeftButtonDown = true;
+         _dragAnchorPoint = e.GetPosition( this ) - Offset;
+      }
+
+      protected override void OnMouseMove( MouseEventArgs e )
+      {
+         base.OnMouseMove( e );
+
+         if (_isLeftButtonDown)
+         {
+            Offset = e.GetPosition( this ) - _dragAnchorPoint;
+         }
+      }
+
+      protected override void OnMouseLeftButtonUp( MouseButtonEventArgs e )
+      {
+         base.OnMouseLeftButtonUp( e );
+         _isLeftButtonDown = false;
       }
 
       protected override void OnRender( DrawingContext dc )
@@ -35,8 +75,10 @@ namespace DragonQuestinoEditor
             int width = (int)tile.Image.Width;
             int height = (int)tile.Image.Height;
 
-            int x = (i % 140) * width;
-            int y = (i / 140) * height;
+            var offset = Offset;
+
+            int x = (int)offset.X + (i % 140) * width;
+            int y = (int)offset.Y + (i / 140) * height;
 
             dc.DrawImage( tile.Image, new Rect( x, y, width, height ) );
          }
