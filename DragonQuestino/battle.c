58c699de00d6e829b983a8fb4542c72c9083f8a0
selgas <s.elgas@techsmith.com>
2025-05-16 14:06:49 -0400

    Fixed a couple bugs with enemy pool indexing

--- DragonQuestino/battle.c
+++ DragonQuestino/battle.c
@@ -17,17 +17,18 @@ void Battle_Generate( Battle_t* battle )
 {
    Player_t* player = battle->player;
    TileMap_t* tileMap = battle->tileMap;
-   uint32_t enemyPoolIndex = TILE_GET_ENEMYPOOLINDEX( tileMap->tiles[player->tileIndex] );
+   uint32_t adjustedTileIndex = player->tileIndex + ( ( player->tileIndex / tileMap->tilesX ) * ( TILE_COUNT_X - tileMap->tilesX ) );
+   uint32_t enemyPoolIndex = TILE_GET_ENEMYPOOLINDEX( tileMap->tiles[adjustedTileIndex] );
    uint32_t i, enemyIndex;
 
    if ( tileMap->isDungeon )
    {
-      i = Random_u32( 0, tileMap->dungeonEnemyIndexPools[enemyPoolIndex].enemyCount );
+      i = Random_u32( 0, tileMap->dungeonEnemyIndexPools[enemyPoolIndex].enemyCount - 1 );
       enemyIndex = tileMap->dungeonEnemyIndexPools[enemyPoolIndex].enemyIndexes[i];
    }
    else
    {
-      i = Random_u32( 0, tileMap->overworldEnemyIndexPools[enemyPoolIndex].enemyCount );
+      i = Random_u32( 0, tileMap->overworldEnemyIndexPools[enemyPoolIndex].enemyCount - 1 );
       enemyIndex = tileMap->overworldEnemyIndexPools[enemyPoolIndex].enemyIndexes[i];
    }
 
