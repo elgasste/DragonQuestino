e6c4404af894eeef71e97f94f32392e8865fd2e1
selgas <s.elgas@techsmith.com>
2025-08-07 17:27:51 -0400

    Password Generator populates combo boxes

--- DragonQuestinoPasswordGenerator/DragonQuestinoPasswordGenerator/ViewModels/MainWindowViewModel.cs
+++ DragonQuestinoPasswordGenerator/DragonQuestinoPasswordGenerator/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,87 @@
-﻿namespace DragonQuestinoPasswordGenerator.ViewModels
+﻿using System.Collections.ObjectModel;
+using System.Numerics;
+
+namespace DragonQuestinoPasswordGenerator.ViewModels
 {
+   public class BattleItem( string displayText, int shift)
+   {
+      public string DisplayText { get; set; } = displayText;
+      public int Shift { get; } = shift;
+   }
+
    public class MainWindowViewModel : ViewModelBase
    {
+      public ObservableCollection<BattleItem> Weapons { get; set; } =
+      [
+         new( "none", 0 ),
+         new( "bamboo pole", 1 ),
+         new( "club", 2 ),
+         new( "copper sword", 3 ),
+         new( "hand axe", 4 ),
+         new( "broad sword", 5 ),
+         new( "flame sword", 6 ),
+         new( "Erdrick's Sword", 7 )
+      ];
+
+      private BattleItem _selectedWeapon;
+      public BattleItem SelectedWeapon
+      {
+         get => _selectedWeapon;
+         set
+         {
+            if ( SetProperty( ref _selectedWeapon, value ) )
+            {
+               UpdatePassword();
+            }
+         }
+      }
+
+      public ObservableCollection<BattleItem> Armor { get; set; } =
+      [
+         new( "none", 0 ),
+         new( "clothes", 1 ),
+         new( "leather armor", 2 ),
+         new( "chain mail", 3 ),
+         new( "half plate", 4 ),
+         new( "full plate", 5 ),
+         new( "Magic Armor", 6 ),
+         new( "Erdrick's Armor", 7 )
+      ];
+
+      private BattleItem _selectedArmor;
+      public BattleItem SelectedArmor
+      {
+         get => _selectedArmor;
+         set
+         {
+            if ( SetProperty( ref _selectedArmor, value ) )
+            {
+               UpdatePassword();
+            }
+         }
+      }
+
+      public ObservableCollection<BattleItem> Shields { get; set; } =
+      [
+         new( "none", 0 ),
+         new( "small shield", 1 ),
+         new( "large shield", 2 ),
+         new( "silver shield", 3 )
+      ];
+
+      private BattleItem _selectedShield;
+      public BattleItem SelectedShield
+      {
+         get => _selectedShield;
+         set
+         {
+            if ( SetProperty( ref _selectedShield, value ) )
+            {
+               UpdatePassword();
+            }
+         }
+      }
+
       private string _playerName = string.Empty;
       public string PlayerName
       {
@@ -106,6 +186,13 @@
          }
       }
 
+      public MainWindowViewModel()
+      {
+         _selectedWeapon = Weapons[0];
+         _selectedArmor = Armor[0];
+         _selectedShield = Shields[0];
+      }
+
       private void UpdatePassword()
       {
          string name = ( _playerName == string.Empty ) ? " " : _playerName.Substring( 0, 8 );
@@ -116,6 +203,9 @@
          int torches = GetRangedNumberFromString( _torchCount, 7 );
          int wings = GetRangedNumberFromString( _wingCount, 7 );
          int fairyWaters = GetRangedNumberFromString( _fairyWaterCount, 7 );
+         int weaponShift = SelectedWeapon.Shift;
+         int armorShift = SelectedArmor.Shift;
+         int shieldShift = SelectedShield.Shift;
       }
 
       private int GetRangedNumberFromString( string str, int max )
