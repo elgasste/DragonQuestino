7d803e3193b8fb720f228be3d07d9fd3e46206be
selgas <s.elgas@techsmith.com>
2025-08-07 15:45:18 -0400

    Password generator checks numbers and copies to clipboard

--- DragonQuestinoPasswordGenerator/DragonQuestinoPasswordGenerator/MainWindow.xaml.cs
+++ DragonQuestinoPasswordGenerator/DragonQuestinoPasswordGenerator/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using DragonQuestinoPasswordGenerator.ViewModels;
+using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace DragonQuestinoPasswordGenerator
 {
@@ -10,6 +13,50 @@ namespace DragonQuestinoPasswordGenerator
          InitializeComponent();
 
          DataContext = new MainWindowViewModel();
+      }
+
+      private void TextBox_PreviewTextInput_NumericOnly( object sender, TextCompositionEventArgs e )
+      {
+         Regex regex = new Regex( "^\\d+$" );
+         e.Handled = !regex.IsMatch( e.Text );
+      }
+
+      private void TextBox_PreviewKeyDown_NoSpaces( object sender, KeyEventArgs e )
+      {
+         e.Handled = e.Key == Key.Space;
+      }
+
+      private void TextBox_TextChanged_MaxUint16( object sender, TextChangedEventArgs e )
+      {
+         if ( sender is TextBox textBox )
+         {
+            if ( int.TryParse( textBox.Text, out int value ) )
+            {
+               if ( value > UInt16.MaxValue )
+               {
+                  textBox.Text = UInt16.MaxValue.ToString(); // Set to max if exceeded
+               }
+            }
+         }
+      }
+
+      private void TextBox_TextChanged_Max7( object sender, TextChangedEventArgs e )
+      {
+         if ( sender is TextBox textBox )
+         {
+            if ( int.TryParse( textBox.Text, out int value ) )
+            {
+               if ( value > 7 )
+               {
+                  textBox.Text = "7"; // Set to max if exceeded
+               }
+            }
+         }
+      }
+
+      private void CopyPasswordButton_Click( object sender, RoutedEventArgs e )
+      {
+         Clipboard.SetText( PasswordTextBox.Text );
       }
    }
 }
