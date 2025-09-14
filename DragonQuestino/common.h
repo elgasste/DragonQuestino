89dc1c6b23ec1fe091fc507b5c9082ce080977ca
selgas <s.elgas@techsmith.com>
2025-09-14 17:45:17 -0400

    typedef 64-bit types

--- DragonQuestino/common.h
+++ DragonQuestino/common.h
@@ -33,10 +33,12 @@
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
+typedef uint64_t u64;
 typedef int8_t i8;
 typedef int16_t i16;
 typedef int32_t i32;
 typedef float r32;
+typedef double r64;
 
 #define Bool_t i32
 #define True 1
