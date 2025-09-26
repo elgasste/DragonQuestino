30868402a23642e94fbc6b0cf7196c039af26283
selgas <s.elgas@techsmith.com>
2025-09-26 16:05:56 -0400

    Outro player name should be upper-case

---
+++ DragonQuestino/string_util.h
@@ -0,0 +1,16 @@
+#if !defined( STRING_UTIL_H )
+#define STRING_UTIL_H
+
+#include "common.h"
+
+#if defined( __cplusplus )
+extern "C" {
+#endif
+
+void StringUtil_ToUpper( char* str );
+
+#if defined( __cplusplus )
+}
+#endif
+
+#endif // STRING_UTIL_H
